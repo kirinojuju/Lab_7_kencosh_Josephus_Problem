448fb691754e450baabbef795b9f9180463567b8
kirinojuju <pojcharapon_san@cmu.ac.th>
2026-02-06 15:31:18 +0700

    Creating Function

---
+++ hot_potato.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+int main()
+{
+    list <int> lst;
+        int N, M;
+    
+    cout << "Hot Potato Game!" << endl;
+    
+    cout << "Enter the N (number of people): ";
+    cin >> N;
+    cout << "Enter the M (number of turn): ";
+    cin >> M;
+
+    for(int i = 0; i < N; i++){
+        lst.push_back(i+1);
+    }
+
+    cout << "So,.. There are " << N << " person in this Game" << endl;
+    cout << endl;
+
+    list<int>::iterator people = lst.begin();
+
+    for(int i = 0; i < N; i++){
+        cout << "Person: " << *people << " ";
+        people++;
+        if(people == lst.end()){
+            people = lst.begin();
+        }
+    }
+    cout << endl;
+
+    list<int>::iterator current_people = people;
+
+    while(lst.size() > 1){
+        for(int i = 1; i < M+1; i++){
+            current_people++;
+            if(current_people == lst.end()){
+                current_people = lst.begin();
+            }
+        }
+        cout << "Eliminated: " << *current_people << endl;
+
+        lst.erase(current_people);
+    
+    }
+    
+    cout << "The winner is " << lst.front() << endl;
+
+    return 0;
+}
